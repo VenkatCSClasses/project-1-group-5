e7182f42d81593f25ab0ae9b714101683448eac6
faithowino <fowino@ithaca.edu>
2026-03-05 10:12:44 -0500

    Created the Bank Account Interface

--- src/main/java/bank/BankAccount.java
+++ src/main/java/bank/BankAccount.java
@@ -1,54 +1,31 @@
 package bank;
 import java.util.List;
 
-public class BankAccount {
-    int customerID;
-    int accountNumber;
-    double balance;
-    boolean isFrozen;
-    List<Transaction> transactionHistory;
 
-    public BankAccount(int customerID, int accountNumber, double initialDeposit){
-        throw new IllegalArgumentException("Not implemented yet");
-    }
+public interface BankAccount {
+    void deposit(double amount);
 
-    public void deposit(double amount){
-        throw new IllegalArgumentException("Not implemented yet");
-    }
+    void withdraw(double amount);
 
-    public void withdraw(double amount){
-        throw new IllegalArgumentException("Not implemented yet");
-    }
+    void transfer(BankAccount targetAccount, double amount);
 
-    public void transfer(BankAccount targetAccount, double amount){
-        throw new IllegalArgumentException("Not implemented yet");
-    }
+    double checkBalance();
 
-    public double getBalance(){
-        throw new IllegalArgumentException("Not implemented yet");
-    }
+    List<Transaction> getSuspiciousActivity();
 
-    public List<Transaction> getTransactionHistory(){
-        throw new IllegalArgumentException("Not implemented yet");
-    }
-
-    public List<Transaction> getSuspiciousActivityReport(){
-        throw new IllegalArgumentException("Not implemented yet");
-    }
-
-    public boolean isFrozen(){
-        throw new IllegalArgumentException("Not implemented yet");
-    }
-
-    public int getAccountNumber(){
-        throw new IllegalArgumentException("Not implemented yet");
-    }
-
-    public int getCustomerID(){
-        throw new IllegalArgumentException("Not implemented yet");
-    }
+    List<Transaction> getTransactionHistory();
 
 
+
+
+
+
+
+
+
+
+    
+    
 
 
 
