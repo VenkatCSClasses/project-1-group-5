3a9a8d9e47dc07e349e0a1907f2f8dcd1352600f
baneetpukhrambam <bpukhrambam@ithaca.edu>
2026-03-06 09:56:11 -0500

    Added processTransaction for BankAdmin

--- src/main/java/bank/BankAdmin.java
+++ src/main/java/bank/BankAdmin.java
@@ -11,7 +11,13 @@ public class BankAdmin {
     }
 
     public void processTransaction(int accountNumber, double amount, int transactionType){
-        throw new IllegalArgumentException("Not implemented yet");
+       if transactionType == 1 {
+           Bank.bankAccounts.get(accountNumber).deposit(amount);
+       } else if (transactionType == 2) {
+           Bank.bankAccounts.get(accountNumber).withdraw(amount);
+       } else {
+           throw new IllegalArgumentException("Invalid transaction type");
+       }
     }
 
     public double calculateTotalAssets(){
