3512f376f801c2f519515f3d2b34565559ba7a09
baneetpukhrambam <bpukhrambam@ithaca.edu>
2026-03-10 14:33:55 -0400

    added BankTeller CLI code

--- src/main/java/bank/Cli.java
+++ src/main/java/bank/Cli.java
@@ -372,10 +372,156 @@ public class Cli{
 
 
              
-            } else if (choice.equals("3")){
-                System.out.println("Exiting the CLI. Goodbye!");
-                break;
-            }
+            } else if (choiceInt == 3){
+                System.out.println("Teller Login selected.");
+                // Teller menu
+
+                System.out.println("What would you like to do? Select a number:");
+                System.out.println("1. Create Account");
+                System.out.println("2. Close Account");
+                System.out.println("3. Process Transaction");       
+                System.out.println("4. Log out");
+                String tellerChoice = scanner.nextLine();
+    
+
+                while(tellerChoice != "1" && tellerChoice != "2" && tellerChoice != "3" && tellerChoice != "4"){
+                    System.out.println("Invalid choice. Please try again.");
+                    System.out.println("What would you like to do? Select a number:");
+                    System.out.println("1. Create Account");
+                    System.out.println("2. Close Account");
+                    System.out.println("3. Process Transaction");       
+                    System.out.println("4. Log out");
+                    tellerChoice = scanner.nextLine();
+
+                }
+                int tellerChoiceInt = Integer.parseInt(tellerChoice);
+
+                if(tellerChoiceInt == 1){
+                    System.out.println("Create Account selected.");
+                    // Create account logic here
+
+                    System.out.println("Enter customer ID for the new account:");
+                    String customerId = scanner.nextLine();
+
+                    while(true){ //while loop to validate that the customer ID is an integer
+                        try{
+                            int tellerCustomerId = Integer.parseInt(customerId);
+                            break; //break out of the loop if parsing is successful
+                        }catch(NumberFormatException e){
+                            System.out.println("Invalid customer ID. Must be an integer.");
+                            System.out.println("Enter customer ID for the new account:");
+                            customerId = scanner.nextLine();
+                        }
+                    }
+
+                    System.out.println("Enter initial deposit amount:");
+                    String initialDeposit = scanner.nextLine();
+
+
+                    while(true){ //while loop to validate that the initial deposit is a double
+                        try{
+                            Double amountDouble = Double.parseDouble(initialDeposit);
+                            break; //break out of the loop if parsing is successful
+                        }catch(NumberFormatException e){
+                            System.out.println("Invalid amount. Must be a number.");
+                            System.out.println("Enter initial deposit amount:");
+                            initialDeposit = scanner.nextLine();
+                        }
+                    }
+                    
+
+
+                    System.out.println("Enter account type (1 for Checking, 2 for Savings):");
+                    String accountType = scanner.nextLine();
+                    int accountTypeInt = Integer.parseInt(accountType);
+
+                    // Validate account type
+                    while(accountTypeInt != 1 && accountTypeInt != 2){
+                        System.out.println("Invalid choice. Please try again.");
+                        System.out.println("Enter account type (1 for Checking, 2 for Savings):");
+                        accountType = scanner.nextLine();
+                        accountTypeInt = Integer.parseInt(accountType);
+                    }
+
+                    BankTeller.createAccount(tellerCustomerId, initialDepositDouble, accountTypeInt);
+                }else if(tellerChoiceInt == 2){
+                    System.out.println("Close Account selected.");
+                    // Create account logic here
+
+                    System.out.println("Enter account number to close:");
+                    String accountNumber = scanner.nextLine();
+
+                    while(true){ //while loop to validate that the account number is an integer
+                        try{
+                            int tellerAccountNumber = Integer.parseInt(accountNumber);
+                            break; //break out of the loop if parsing is successful
+                        }catch(NumberFormatException e){
+                            System.out.println("Invalid account number. Must be an integer.");
+                            System.out.println("Enter account number to close:");
+                            accountNumber = scanner.nextLine();
+                        }
+                    }
+
+                    BankTeller.closeAccount(tellerAccountNumber);
+                }else if(tellerChoiceInt == 3){
+                    System.out.println("Process Transaction selected.");
+                    // Process transaction logic here
+                    System.out.println("Enter an account number to access:");
+                        String accountNum = scanner.nextLine();
+                        int accountNumInt = Integer.parseInt(accountNum);       
+
+                        // Validate account number
+                        //while the account number is not in the customer's accounts list, ask for a valid account number
+                        while(!customerAccounts.contains(customerAccounts.stream().filter(a -> a.getAccountNumber() == accountNumInt).findFirst().orElse(null))){
+                            System.out.println("Invalid account number. Please try again.");
+                            System.out.println("Enter an account number to access:");
+                            accountNum = scanner.nextLine();
+                            accountNumInt = Integer.parseInt(accountNum);
+                        }
+
+                        //Selected Account
+                        BankAccount selectedAccount = customerAccounts.stream().filter(a -> a.getAccountNumber() == accountNumInt).findFirst().orElse(null);
+                        System.out.println("Account selected: " + selectedAccount); 
+
+                        //Once a valid account number is entered..
+                        System.out.println("What would you like to do? Select a number:");
+                        System.out.println("1. Deposit");
+                        System.out.println("2. Withdraw");
+                        System.out.println("3. Log out");
+                        String customerChoice = scanner.nextLine();
+    
+                        // Validate customer choice
+                        while(customerChoice != "1" && customerChoice != "2" && customerChoice != "3"){
+                            System.out.println("Invalid choice. Please try again.");
+                            System.out.println("What would you like to do? Select a number:");
+                            System.out.println("1. Deposit");
+                            System.out.println("2. Withdraw");
+                            System.out.println("3. Log out");
+                            customerChoice = scanner.nextLine();
+                        }
+
+                        int customerChoiceInt = Integer.parseInt(customerChoice);
+
+                        // Perform the selected action
+                        if (customerChoiceInt == 1) {
+                            System.out.println("Enter amount to deposit:");
+                            String amount = scanner.nextLine();
+                            double amountDouble = Double.parseDouble(amount);
+                            Atm.processTransaction(selectedAccount, amountDouble, 1);
+                            System.out.println("Deposit successful! New balance: $" + String.format("%.2f", selectedAccount.checkBalance()));
+                        } else if (customerChoiceInt == 2) {
+                            System.out.println("Enter amount to withdraw:");
+                            String amount = scanner.nextLine();
+                            double amountDouble = Double.parseDouble(amount);           
+                            Atm.processTransaction(selectedAccount, amountDouble, 0);
+                            System.out.println("Withdrawal successful! New balance: $" + String.format("%.2f", selectedAccount.checkBalance()));
+                        } else if (customerChoiceInt == 3) {
+                            System.out.println("Logging out...");
+                            break;
+                        }
+
+                    }    
+                } 
 
 
 
